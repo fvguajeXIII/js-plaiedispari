e8d905dddacc796b9bc53f93201b21c9eabc11d7
fvguajexiii <f.vellucci13@outlook.it>
2025-07-07 14:54:36 +0200

    Ritorno del valore inserito

--- js/script.js
+++ js/script.js
@@ -2,3 +2,9 @@
 
 //Inserimento da parte dell'utente della parola
 const parola = prompt(`Inserisci una parola`);
+
+//Funzione per verificare se una parola è palindroma
+function isPalindroma(stringa) {
+  //ritorno del valore con i metodi reverse per l'inversione, split per trasformare la stringa in array, join per ricomporre la parola
+  return stringa === stringa.split(``).reverse().join(``);
+}
